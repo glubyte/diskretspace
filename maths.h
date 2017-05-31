dcbaa6f276dd25fe9c6621ee9a3df8746ea7af06
glubyte <danadelconte91@gmail.com>
2017-05-30 20:45:00 -0700

    Add files via upload

--- maths.h
+++ maths.h
@@ -43,13 +43,14 @@ typedef struct {
 } vec3;
 
 // prototypes
+
 // linear algebra
 float dot(vec3 a, vec3 b);
 vec3 cross(vec3 a, vec3 b);
 float length(vec3 a);
 void normalize(vec3* a);
 
-// ai math
+// symbolic computation
 char* mathLex(char* expression);
 token* genTokens(char* expression);
 node* genTree(token* tokens);
